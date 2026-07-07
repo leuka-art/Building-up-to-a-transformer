9ef7d8717c634e6c522015e79a5f64572ece8723
leuka-art <leukaye0618@gmail.com>
2026-07-07 15:40:49 +0100

    Create Model.py

---
+++ MLP/Model.py
@@ -0,0 +1,39 @@
+import torch
+
+class Linear:
+    def __init__(self,nin,nout):
+        self.weights = torch.randn(nin,nout) * (2/(nin+nout))**0.5
+        self.weights.requires_grad_()
+        self.bias = torch.zeros(nout)
+        self.bias.requires_grad_()
+    def forward(self,x):
+        return x@self.weights+self.bias
+    def parameters(self):
+        return [self.weights,self.bias]
+
+class ReLU:
+    def forward(self,x):
+        return torch.relu(x)
+    def parameters(self):
+        return []
+    
+class Tanh:
+    def forward(self,x):
+        return torch.tanh(x)
+    def parameters(self):
+        return []
+
+class Sequential:
+    def __init__(self,layers):
+        self.layers=layers
+    def forward_prop(self,x):
+        y=x
+        for layer in self.layers:
+            y=layer.forward(y)
+        return y
+    def parameters(self):
+        mylist=[]
+        for layer in self.layers:
+            p=layer.parameters()
+            mylist.extend(p)
+        return mylist
